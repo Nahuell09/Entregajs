a281c837e84f7988f2423586046ec98f1a87e868
Nahuel Aaron Rojas <aronnahuelrojas@gmail.com>
2022-07-18 20:18:27 -0300

    Sumamos unos codigos en js

--- tarea.js
+++ tarea.js
@@ -30,3 +30,77 @@ class Figuras {
         return this.precioFinalCuotas;
     }
 };
+
+localStorage.clear();
+
+const productos = [];
+let carrito =[];
+
+const figura1 = new Figuras(1, "lionel Messi",10000, 5);
+const figura2 = new Figuras(2, "Cristiano Ronaldo", 9000,5);
+const figura3 = new Figuras(3, "Kilian Mbappe", 5000, 5);
+const figura4 = new Figuras(4, "Karim Benzema", 7000, 5);
+
+
+productos.push(figura1, figura2, figura3, figura4);
+
+
+const divContenedor = document.getElementById("section");
+
+
+for(const producto of productos){
+  
+    const div = document.createElement("div");
+   
+    div.className = "col w-100 d-flex d-flex justify-content-evenly ";
+   
+    div.innerHTML = `
+                <h3 class="fs-4">Producto: ${producto.name}</h3>
+                <h3 class="fs-5">Precio: ${producto.precio}</h3>
+                <p class="fs-5">Seleccione la cantidad de cuotas: 
+                    <select id=${producto.id}>
+                        <option value="1">Seleccione las cuotas</option>
+                        <option value="3">3 cuotas</option>
+                        <option value="6">6 cuotas</option>
+                        <option value="12">12 cuotas</option>
+                    </select>
+                </p>
+                <button type="button" id=boton-${producto.id} class="btn btn-outline-black">Agregar al carrito</button>
+                <p id=precio-${producto.id}>El precio de cada cuota es: $ ${producto.precioFinalCuotas}</p>
+                `;
+       
+        divContenedor.append(div);
+
+       
+        const select = document.getElementById(`${producto.id}`);
+      
+        select.addEventListener("change", () => {
+           
+            cambioCuotas(select.value, producto);  
+        });
+
+        
+        const boton = document.getElementById(`boton-${producto.id}`);
+        boton.addEventListener("click", () => {
+            
+
+      
+            const obtenerCarrito = localStorage.getItem("carrito")
+            
+            if(obtenerCarrito){
+                carrito = JSON.parse(obtenerCarrito);
+            }
+        
+            carrito.push(producto);
+            const carritoStorage = JSON.stringify(carrito);
+            localStorage.setItem("carrito", carritoStorage);
+            console.log("El producto fue añadido")
+        });
+};
+
+
+function cambioCuotas(cuota, producto){
+    producto.cuotas = cuota;
+    const precioFinalCuotas = producto.calcularPrecioCuotas();
+    document.getElementById(`precio-${producto.id}`).innerHTML = `El precio de cada cuota es: $ ${precioFinalCuotas}`
+};
